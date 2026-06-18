b662406e7cd3b6d3166de756b6f6bc3fc78d3263
P_Pkaifan <Kaifan152159@gmail.com>
2026-06-18 18:31:10 +1000

    Add GitHub Actions workflow for deploying to GitHub Pages

--- app/grammar/[unitId]/page.tsx
+++ app/grammar/[unitId]/page.tsx
@@ -1,10 +1,14 @@
-"use client";
-
 import { use } from "react";
 import { notFound } from "next/navigation";
 import { getUnitData } from "@/lib/unit-data-registry";
 import { getModuleByUnitId } from "@/lib/grammar-data";
 import UnitPage from "@/components/grammar/UnitPage";
+
+const ALL_UNITS = Array.from({ length: 42 }, (_, i) => `unit-${String(i + 1).padStart(2, "0")}`);
+
+export function generateStaticParams() {
+  return ALL_UNITS.map((unitId) => ({ unitId }));
+}
 
 export default function UnitRoute({
   params,
